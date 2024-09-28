03246a644ace0efadcb55b9a6f97c11f5fef2ca9
FarisRamadhan <141473374+frinde@users.noreply.github.com>
2024-09-28 21:38:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+    nama: <input type="text" name="nama" id="nama">
+    <input type="submit">
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $nama = htmlspecialchars($_POST['nama']);
+    if ($nama == "antartika2") {
+        header("https://www.google.com");
+    } else {
+        echo "nama salah!".PHP_EOL;
+    }
+}
+?>
+
+</body>
+</html>
